b81bc52806aa03847c707a7dd9b9c9758cdecc0d
Yahya Ustunay <yahyaustunay@gmail.com>
2019-12-20 23:41:56 +0000

    index.js changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -201,7 +201,7 @@ export default class IndexPage extends React.Component {
               </div>
               <div className="col-lg-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                <a className="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                <a className="d-block" href="mailto:contact@overrideltd.com">contact@overrideltd.com</a>
               </div>
             </div>
           </div>
