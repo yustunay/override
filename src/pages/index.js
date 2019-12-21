c3ba2f511b22f5b50adaf5532621791f54e0d0cc
Yahya Ustunay <yahyaustunay@gmail.com>
2019-12-21 20:34:40 +0000

    mail address corrected

--- src/pages/index.js
+++ src/pages/index.js
@@ -190,7 +190,7 @@ export default class IndexPage extends React.Component {
               </div>
               <div className="col-lg-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                <a className="d-block" href="mailto:contact@overrideltd.com">contact@overrideconsulting.com</a>
+                <a className="d-block" href="mailto:contact@overrideconsulting.com">contact@overrideconsulting.com</a>
               </div>
             </div>
           </div>
