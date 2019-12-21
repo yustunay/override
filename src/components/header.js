9a12f488e4ce3bd6d47f6b890c740b0c36c2745b
Yahya Ustunay <yahyaustunay@gmail.com>
2019-12-21 16:35:02 +0000

    header.js data edited

--- src/components/header.js
+++ src/components/header.js
@@ -43,13 +43,12 @@ export default class Header extends React.Component {
           <div className="container h-100">
             <div className="row h-100 align-items-center justify-content-center text-center">
               <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
+                <h1 className="text-uppercase text-white font-weight-bold">Software Consultancy
                   Themes</h1>
                 <hr className="divider my-4"/>
               </div>
               <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
-                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
+                <p className="text-white-75 font-weight-light mb-5">Override is the Software Consulting Company in UK , with a solid experience in providing Technology Solutions to companies of all sizes, from startups to global corporations.</p>
                 <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Find Out More</a>
               </div>
             </div>
